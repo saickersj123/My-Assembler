d3186f9aa88abb2550a505ec1cbb63cf6de38d7f
saickersj123 <150012248+saickersj123@users.noreply.github.com>
2023-11-08 13:14:21 +0900

    fda

--- my_assembler_20194318.c
+++ my_assembler_20194318.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>	
 #include <string.h>
-#include <malloc.h>
 #include "my_assembler_20194318.h"
 
 int init_my_assembler(void){
@@ -13,26 +12,42 @@ int init_inst_file(uchar *inst_file){
     FILE *inst = fopen(inst_file, "r");
     inst_index = 0;
     int i;
+    printf("Instruction File Initialized\n");
     while(!feof(inst)){
-        fgets(inst_table, sizeof(inst_table)-1,inst);
+        fgets(inst_table, sizeof(inst_table),inst);
         inst_index++;
         printf("Inst Line [%d] : %s", inst_index, inst_table);
     }
+    printf("\n");
     fclose(inst);
 }
+
 int init_input_file(uchar *input_file){
     FILE *inp = fopen(input_file,"r");
     line_num = 0;
+    printf("Input File Initialized \n");
     while(!feof(inp)){
-        fgets(input_data, sizeof(input_data)-1,inp);
+        fgets(input_data, sizeof(input_data),inp);
         line_num++;
-        printf("\nInput Line [%d] : %s", line_num, input_data);
+        printf("Input Line [%d] : %s", line_num, input_data);
 
     }
+    printf("\n");
     fclose(inp);
 }
 
-int token_parsing(uchar *str);
+int token_parsing(uchar *str)
+{
+    int i;
+    uchar cpy[MAX_LINES];
+    while(input_data == EOF ){
+            const char delim[] = " ";
+            str = strtok(input_data,delim);
+            fgets(token_table,sizeof(token_table),str);
+            printf("%s\n",token_table);
+    }
+}
+
 int search_opcode(uchar *str);
 static int assem_pass1(void);
 void make_opcode_output(uchar *file_name);
@@ -43,4 +58,6 @@ void make_objectcode_output(uchar *file_name);
 
 int main(){
     init_my_assembler();
+    token_parsing(input_data);
+    return 0;
 }
