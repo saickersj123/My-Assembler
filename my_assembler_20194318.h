7063fd043592b8c02d8e225183626c6e315c3771
username <saickersj123@gmail.com>
2023-12-21 03:28:05 +0900

    added comments
    231221

--- my_assembler_20194318.h
+++ my_assembler_20194318.h
@@ -1,7 +1,11 @@
 /* 
- * my_assembler 함수를 위한 변수 선언 및 매크로를 담고 있는 헤더 파일이다. 
- * 
+ * Header file containing declarations and macros for the my_assembler_20194318.c
  */
+#include <stdio.h>
+#include <stdlib.h>	
+#include <string.h>
+#include <ctype.h>
+
 #define MAX_INST 256
 #define MAX_LINES 5000
 #define MAX_OPERAND 3
@@ -9,14 +13,14 @@
 typedef unsigned char uchar;
 
 /* 
- * instruction 목록 파일로 부터 정보를 받아와서 생성하는 구조체 변수이다.
- * 라인 별로 하나의 instruction을 저장한다.
+ * Structure to store information from the instruction list file
+ * Represents one instruction per line
  */
 struct inst_unit
 {
 	uchar str[10];
-	int ops; //	0:-		1:M		2:R		3:N		4:RR	5:RN
-	int format; // 0:not SIC/XE code	1:format 1	2:format 2		3:format 3/4
+	//int ops; //	0:-		1:M		2:R		3:N		4:RR	5:RN
+	int format; // 0:directive	1:format 1	2:format 2	3:format 3/4
 	uchar op;
 };
 typedef struct inst_unit inst;
@@ -25,15 +29,13 @@ static int inst_index;
 static int inst_count;
 
 /*
- * 어셈블리 할 소스코드를 입력받는 테이블이다. 라인 단위로 관리할 수 있다.
- fopen input.txt
+ * Table to store the source code to be assembled. Managed line by line
  */
 uchar *input_data[MAX_LINES];
 static int line_num;
 
 /* 
- * 어셈블리 할 소스코드를 토큰단위로 관리하기 위한 구조체 변수이다.
- * operator는 renaming을 허용한다.
+ * Structure to manage the source code at the token level
  */
 struct token_unit
 {
@@ -50,80 +52,86 @@ token *token_table[MAX_LINES];
 static int token_line;
 
 /*
- * 심볼을 관리하는 구조체이다.
- * 심볼 테이블은 심볼 이름, 심볼의 위치로 구성된다.
- * 추후 과제에 사용 예정
+ * Structure to manage symbols
+ * Symbol table consists of symbol name and its location
+ * and Section
  */
+
 struct symbol_unit
 {
 	uchar symbol[10];
-	int addr;
-	int sec; // section
+	int addr; // Locctr
+	int sec; // Section
 };
 
 typedef struct symbol_unit symbol;
-symbol sym_table[MAX_LINES];
-static int sym_index;
-static int locctr; // loc값 
+symbol sym_table[MAX_LINES]; // Symbol table
+static int sym_index; // Symbol index
+static int locctr; // Location counter
 
-static uchar *input_file; // 입력 파일 input.txt
-static uchar *output_file;// 출력 파일 output.txt
+int init_my_assembler(void); // Initialize the assembler, includes init_inst_file() and init_input_file()
+int init_inst_file(uchar *inst_file); // Read inst.data and store in inst_table[]
+int init_input_file(uchar *input_file); // Read input.txt and store in input_data[]
+int token_parsing(uchar *str); // Tokenize lines stored in input_data[] and store in token_table[]
+int search_opcode(uchar *str); // Search opcode from inst_table[] returns opcode index
+static int assem_pass1(void); // Process Pass 1
+void make_opcode_output(uchar *file_name); // Make op table file
+void make_symtab_output(uchar *file_name); // Make symbol table file
+static int assem_pass2(void); // Process Pass 2
+void make_objectcode_output(uchar *file_name, uchar *list_name); // Make object program and list file
 
-int init_my_assembler(void); // 어셈블러 초기화 파일 init_inst_file()과 init_input_file()도 포함
-int init_inst_file(uchar *inst_file); //inst.data를 읽고 inst_table[]에 저장
-int init_input_file(uchar *input_file); //intput.txt를 읽고 input_data[]에 저장
-int token_parsing(uchar *str); //input_data[]에 저장된 명령어를 토큰화하여 token_table[]에 저장
-int search_opcode(uchar *str); //input_data[]에 저장된 명령어의 opcode를 검색 inst_table[]을 참조
-static int assem_pass1(void); //SIC/XE Pass1 make_opcode_output과 make_symtab_output을 포함
-void make_opcode_output(uchar *file_name); 
-void make_symtab_output(uchar *file_name); 
-static int assem_pass2(void); // SIC/XE Pass2 make_objectcode_output을 포함
-void make_objectcode_output(uchar *file_name, uchar *list_name); // 파일 output.txt에 입력된 명령어의 오브젝트 코드 생성
+// Additional functions and variables
+void write_intermediate_file(uchar *str, int locctr); // Make and write locctr and input line to intermediate file
+void add_to_symtab(const uchar *label, int loc, int is_equ, int sec); // Add symbol to sym_table[]
+int search_symtab(uchar *symbol, int section); // Search symbol from sym_table[] returns address
+int init_token_table(void); // Initialize token_table[]
+int evaluate_expression(uchar *expr); // Evalutate expression ex) BUFEND-BUFFER returns result
+int search_literal(uchar *operand); // Search literal from LTtab[] returns address
+int calculate_byte_length(uchar *operand); // Calculate byte length of operand returns the length
+int search_extRtab(uchar *symbol, int section); // Search external references of current section from extRef[] returns address
+int search_extDtab(uchar *symbol); //  Search external definition from extDef[] returns address
+void handle_extdef(uchar *symbol);// Handle EXTDEF: add symbol, address to extDef[]
+void handle_extref(uchar *symbol, int section); // Handle EXTREF: add symbol, address to extRef[]
+void handle_equ_directive(uchar *label, uchar *operand); // Handle EQU: update locctr, includes evaluate_expression()
+void handle_ltorg_directive(void);// Handle LTORG: add literals of last section to LTtab[], update locctr
+int hexstr2dec(char H);// Convert hex string to dec
+int getREGnum(uchar *register_name);// Return register number
+int generate_object_code(int format);// Generate object code by inst format and operands
+int write_listing_line(int format);// Write line number, locctr, input line, object code to list file by format
+void write_text_record(void);// Write text record to object program
+int write_literal(void);// Write literal to list file
+int handle_pass2(void);// Handle pass2 by directives: write records and listings
 
-//these are extra functions, variables
-void write_intermediate_file(uchar *str, int locctr);
-void add_to_symtab(const uchar *label, int loc, int is_equ, int sec);
-int search_symtab(uchar *symbol, int section);
-int tok_search_opcode(uchar *str);
-int init_token_table(void);
-int evaluate_expression(uchar *expr);
-int search_literal(uchar *operand);
-int calculate_byte_length(uchar *operand);
-int search_extRtab(uchar *symbol, int section);
-int search_extDtab(uchar *symbol);
-void handle_extdef(uchar *symbol);
-void handle_extref(uchar *symbol, int section);
-void handle_equ_directive(uchar *label, uchar *operand);
-void handle_ltorg_directive(void);
-int getREGnum(uchar *register_name);
-int generate_object_code(int format);
-int write_listing_line(int format);
-void write_text_record(void);
-int write_literal(void);
-int handlepass2(void);
-
-// Define the structure for the program section
+/*
+ * Structure to manage csects
+ * csect table consists of section and its program length
+ */
 typedef struct {
     int sec;             // Section number
-	//int locctr;          // Location counter
+	//int locctr;        // Location counter
     int program_length;  // Program length
 } csect;
-#define MAX_CSECT 10
-csect csect_table[MAX_CSECT];
-//--------------
 
+#define MAX_CSECT 10 // Maximum number of Section
+csect csect_table[MAX_CSECT];
+int csect_start_address; 
+
+/*
+ * Structure to manage literals
+ * literal table consists of literal and its pool number
+ * And length and address
+ */
 typedef struct {
     uchar name[20];
-    int leng;
-	int value;
-    int addr;// -1 if not assigned an address yet
+    int leng; // length of literal
+	int value; // pool number of literal
+    int addr; // locctr of literal
 } LT;
 
-// Maximum number of literals (adjust as needed)
-#define MAX_LITERALS 100
-LT LTtab[MAX_LITERALS];
+#define MAX_LITERALS 100 // Maximum number of literals
+LT LTtab[MAX_LITERALS]; 
 int LT_num;
-int current_pool;
+int current_pool; // Track current pool number
 
 #define MAX_EXTDEF 10 
 #define MAX_EXTREF 10
@@ -132,9 +140,8 @@ symbol extRef[MAX_EXTDEF];
 int extDefCount;
 int extRefCount;
 
-// Define a counter for object code records
-FILE *object_code_file;
-FILE *listing_file;
+FILE *object_program_file; // object program file
+FILE *listing_file; // list file
 
 static int starting_address;
 static int sec;
@@ -142,12 +149,9 @@ int object_code[20];
 int text_record_start;
 int text_record_ctr;
 int mod_record_count;
-int mod_last;
-int is_lt;
-int csect_start_address;
-int csect_length;
-int BASEADDR;
-int FEI;
+int mod_last; // Track last record count 
+int is_lt; // Check if it is LT
+int BASEADDR; // Base register address
 int is_first_write = 1; //to check first write for intermediate file
 int first_write = 1; //to check first wirte for object / list
 
@@ -156,4 +160,3 @@ uchar sign2;
 uchar text_record[70];
 uchar mod_record[30][20];
 uchar HEXTAB[]= {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-uchar *texp[10];
